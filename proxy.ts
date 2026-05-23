d09e60993d0745b9e6fe6f4fe3ef53f47d01aff1
jemmaaleksanyan28-dotcom <pazokianzahir@gmail.com>
2026-05-24 01:01:52 +0400

    Use proxy and Persian default locale

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,16 @@
 ﻿import createMiddleware from 'next-intl/middleware'
 import { defaultLocale, locales } from './i18n/config'
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   locales,
   defaultLocale,
   localePrefix: 'always',
   localeDetection: false,
 })
+
+export function proxy(request: Request) {
+  return intlMiddleware(request)
+}
 
 export const config = {
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
